6c1a727092d8e67f666ea07157babd02b3672596
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2019-08-05 18:16:19 +0530

    update email tracking

--- admin/assets/email-blast/js/email-blast.js
+++ admin/assets/email-blast/js/email-blast.js
@@ -416,37 +416,37 @@ $(document).ready(function () {
 	$('#datatable-email-tracking').DataTable();
 
 	// Clone Previous Row for filter input
-	// $('#datatable-email-tracking>thead>tr')
-	// 	.clone(true)
-	// 	.appendTo('#datatable-email-tracking thead');
+	$('#datatable-email-tracking>thead>tr')
+		.clone(true)
+		.appendTo('#datatable-email-tracking thead');
 
-	// $('#datatable-email-tracking>thead>tr:eq(1)>th').each(function (i) {
-	// 	var title = $(this).text();
-	// 	if (title.length > 0 && title != 'Action' && title != 'Status') {
-	// 		$(this).html(
-	// 			'<input type="text" placeholder="Search ' + title + '" />'
-	// 		);
-	// 		$('input', this).on('keyup change', function () {
-	// 			if (table.column(i).search() !== this.value) {
-	// 				table
-	// 					.column(i)
-	// 					.search(this.value)
-	// 					.draw();
-	// 			}
-	// 		});
-	// 	}
+	$('#datatable-email-tracking>thead>tr:eq(1)>th').each(function (i) {
+		var title = $(this).text();
+		if (title.length > 0 && title != 'Action' && title != 'Status') {
+			$(this).html(
+				'<input type="text" placeholder="Search ' + title + '" />'
+			);
+			$('input', this).on('keyup change', function () {
+				if (table.column(i).search() !== this.value) {
+					table
+						.column(i)
+						.search(this.value)
+						.draw();
+				}
+			});
+		}
 
 
-	// 	var select = $('<select><option value=""></option></select>')
-	// 		.appendTo($(this).empty())
-	// 		.on('change', function () {
-	// 			table.column(i)
-	// 				.search($(this).val())
-	// 				.draw();
-	// 		});
+		var select = $('<select><option value=""></option></select>')
+			.appendTo($(this).empty())
+			.on('change', function () {
+				table.column(i)
+					.search($(this).val())
+					.draw();
+			});
 
-	// 	table.column(i).data().unique().sort().each(function (d, j) {
-	// 		select.append('<option value="' + d + '">' + d + '</option>')
-	// 	});
-	// });
+		table.column(i).data().unique().sort().each(function (d, j) {
+			select.append('<option value="' + d + '">' + d + '</option>')
+		});
+	});
 });
