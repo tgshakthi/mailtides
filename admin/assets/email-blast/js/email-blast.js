19045d666aa1d267440df0b19902202c9e9d80a8
saravana <31875990+saravanasana@users.noreply.github.com>
2019-08-05 12:56:37 +0530

    Update email-blast.js

--- admin/assets/email-blast/js/email-blast.js
+++ admin/assets/email-blast/js/email-blast.js
@@ -38,7 +38,8 @@ $(document).ready(function() {
 		$.fn.dataTable.ext.search.push(function(settings, data, dataIndex) {
 			var min = $('#min').datepicker('getDate');
 			var max = $('#max').datepicker('getDate');
-			var startDate = new Date(data[3]);
+            var startDate = new Date(data[3]);
+            alert(startDate);
 			if (min == null && max == null) {
 				return true;
 			}
