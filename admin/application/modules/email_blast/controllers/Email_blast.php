eeb4398b2a61cac495f51c97d00da1b6324f6e87
karthika19041991 <43335051+karthika19041991@users.noreply.github.com>
2019-08-05 13:25:37 +0530

    added
    added

--- admin/application/modules/email_blast/controllers/Email_blast.php
+++ admin/application/modules/email_blast/controllers/Email_blast.php
@@ -1520,7 +1520,7 @@ class Email_blast extends MX_Controller
 
       
      
-       $this->table->add_row('<input type="checkbox" class="flat" id="table_records" name="table_records[]" value="' . $get_template->id . '"><input type="hidden" id="row_sort_order" name="row_sort_order[]" value="' . $get_template->id . '">', $get_template->campaign_name,  $status, $cell);
+       $this->table->add_row('<input type="checkbox" class="flat" id="table_records" name="table_records[]" value="' . $get_template->id . '"><input type="hidden" id="row_sort_order" name="row_sort_order[]" value="' . $get_template->id . '">', $get_template->template_name,  $status, $cell);
      }
            
      $template = array(
