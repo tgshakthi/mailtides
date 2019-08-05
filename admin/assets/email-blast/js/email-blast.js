d4b6f291f879cb68707ac369a1e7e728b907782d
saravana <31875990+saravanasana@users.noreply.github.com>
2019-08-05 20:37:39 +0530

    Update email-blast.js

--- admin/assets/email-blast/js/email-blast.js
+++ admin/assets/email-blast/js/email-blast.js
@@ -302,15 +302,15 @@ function campaign(e) {
 			var test = JSON.parse(data);
 			console.log(test);
 
-			var html = '<textarea id="mail-opened" style="display: none">' + opened + '</textarea>' +
-				'<textarea id="mail-unopened" style="display: none">' + not_opened + ' </textarea>' +
-				'<textarea id="mail-comments-posted" style="display: none">' + txgidocs + '</textarea>' +
-				'<textarea id="mail-comments-not-posted" style="display: none">' + google + '</textarea>' +
-				'<textarea id="mail-txgidocs" style="display: none">' + facebook + '</textarea>' +
-				'<textarea id="mail-google" style="display: none">' + sent + '</textarea>' +
-				'<textarea id="mail-facebook" style="display: none">' + posted + '</textarea>' +
-				'<textarea id="mail-sent" style="display: none">' + not_posted + '</textarea>';
-			$('#text_area').html(html);
+			//var html = '<textarea id="mail-opened" style="display: none">' + opened + '</textarea>' +
+			// '<textarea id="mail-unopened" style="display: none">' + not_opened + ' </textarea>' +
+			// '<textarea id="mail-comments-posted" style="display: none">' + txgidocs + '</textarea>' +
+			// '<textarea id="mail-comments-not-posted" style="display: none">' + google + '</textarea>' +
+			// '<textarea id="mail-txgidocs" style="display: none">' + facebook + '</textarea>' +
+			// '<textarea id="mail-google" style="display: none">' + sent + '</textarea>' +
+			// '<textarea id="mail-facebook" style="display: none">' + posted + '</textarea>' +
+			// '<textarea id="mail-sent" style="display: none">' + not_posted + '</textarea>';
+			//$('#text_area').html(html);
 
 
 		}
