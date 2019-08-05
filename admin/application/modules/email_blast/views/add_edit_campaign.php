e0fa6ffcfc601264334dae450bd8c16c9b1a3d68
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2019-08-05 12:03:40 +0530

    update campaign

--- admin/application/modules/email_blast/views/add_edit_campaign.php
+++ admin/application/modules/email_blast/views/add_edit_campaign.php
@@ -90,7 +90,7 @@
 
 								<div class="x_content">
 
-									<div class="form-group">
+									<div class="form-group col-md-6 col-md-6 col-xs-6">
 
 										<?php
 											echo form_label('Campaign Name','name');
