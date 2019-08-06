cd8a655b376f5b450e8e07eccc1416b610498812
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2019-08-06 12:32:10 +0530

    update email blast

--- admin/assets/email-blast/js/email-blast.js
+++ admin/assets/email-blast/js/email-blast.js
@@ -465,15 +465,15 @@ $('#btn').click(function () {
                 var column = this;
                 var select = $('<select><option value=""></option></select>')
                     .appendTo($("#datatable-email").find("th").eq(column.index()))
-                    .on( 'change', function () {
-                        var val = $.fn.dataTable.util.escapeRegex(
-                            $(this).val()
-                        );
- 
-                        column
-                            .search( val ? '^'+val+'$' : '', true, false )
-                            .draw();
-                    } );
+                    .on('change', function () {
+							var val = $.fn.dataTable.util.escapeRegex(
+								$(this).val()
+							);
+							column
+								.search(val ? '^' + val + '$' : '', true, false)
+								.draw();
+						});
+
  
                 column.data().unique().sort().each( function ( d, j ) {
                     select.append( '<option value="'+d+'">'+d+'</option>' )
