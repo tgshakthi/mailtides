8447aa4fc83a076e3d695dc5078c7e88d92821fc
karthika19041991 <43335051+karthika19041991@users.noreply.github.com>
2019-08-06 17:01:38 +0530

    focus
    focus

--- admin/assets/email-blast/js/email-blast.js
+++ admin/assets/email-blast/js/email-blast.js
@@ -521,10 +521,11 @@ $('#campaign-name').blur(function () {
 				} 
 				else 
 				{
-		            $('#error').html('<p style="color:red;font-size: 14px; font-weight:bold;"> Campaign Name Already Exists.</p>');
+					$('#error').html('<p style="color:red;font-size: 14px; font-weight:bold;"> Campaign Name Already Exists.</p>');
+					$('input[name="campaign-name"]').focus();
 				    $('input[name="campaign-name"]').prop('enable', true);
 					document.getElementById("campaign-name").value = "";
-					input.focus();
+					
 					
 				}
 			}
