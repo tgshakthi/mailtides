fd0bbaedabf2ef659a92bd3177ac9392b13e7086
karthika19041991 <43335051+karthika19041991@users.noreply.github.com>
2019-08-06 15:24:18 +0530

    Update add_edit_campaign.php

--- admin/application/modules/email_blast/views/add_edit_campaign.php
+++ admin/application/modules/email_blast/views/add_edit_campaign.php
@@ -134,8 +134,9 @@
 								<h2 class="StepTitle">Campaign Users</h2>
 								
 								<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-								<input type="hidden" name="email_blast_users" id="email_blast_uesrs" value="<?php echo $email_blast_users ;?>">
+							
 									<div class="email-blast-date">
+									<input type="hidden" name="email_blast_users" id="email_blast_uesrs" value="<?php echo $email_blast_users ;?>">
 										<div>
 											<label>From Date:</label>
 											<input type="text" id="min">
