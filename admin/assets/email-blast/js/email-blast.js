b5a01a46b65dfb0412757a127ba8e941628363dc
karthika19041991 <43335051+karthika19041991@users.noreply.github.com>
2019-08-06 17:29:41 +0530

    $(this).focus();
    $(this).focus();

--- admin/assets/email-blast/js/email-blast.js
+++ admin/assets/email-blast/js/email-blast.js
@@ -521,8 +521,9 @@ $('#campaign-name').blur(function () {
 				} 
 				else 
 				{
-					$("input").focus(); 
+					
 					$('#error').html('<p style="color:red;font-size: 14px; font-weight:bold;"> Campaign Name Already Exists.</p>');
+					$(this).focus(); 
 				   $('input[name="campaign-name"]').prop('enable', true);
 					document.getElementById("campaign-name").value = "";
 					
