cf20d362d84e620988ada1325a4eb523f7c6089d
saravana <31875990+saravanasana@users.noreply.github.com>
2019-08-06 18:14:55 +0530

    Update Email_blast.php

--- admin/application/modules/email_blast/controllers/Email_blast.php
+++ admin/application/modules/email_blast/controllers/Email_blast.php
@@ -47,16 +47,20 @@ class Email_blast extends MX_Controller
     function get_table_users()
     {
         $campaign_table = [];
+        $camp = [];
         $website_id = $this->admin_header->website_id();
         $get_users  = $this->Email_blast_model->get_users();
         $campaign_name = "";
         $campaigns = $this->Email_blast_model->get_campaign_name();
         $get_campaigns = $this->Email_blast_model->get_campaign();
+
+        foreach($campaigns as $campaign):        
+          $campaign_table[] =	$campaign->campaign_name;
+        endforeach;
+
+
         foreach (($get_users ? $get_users : array()) as $get_user) {
 
-          echo '<pre>';
-          print_r($campaigns);
-            
             // $anchor_edit = anchor(site_url('email_blast/add_edit_users/' . $get_user->id), '<span class="glyphicon c_edit_icon glyphicon-edit" aria-hidden="true"></span>', array(
             //     'data-toggle' => 'tooltip',
             //     'data-placement' => 'left',
@@ -77,15 +81,25 @@ class Email_blast extends MX_Controller
 		
             $campaign_data = $this->Email_blast_model->get_campaign_data($get_user->id);
 
-            echo '<pre>';
-            print_r($campaign_data);
-            
+            if (in_array($campaign_data[0]->campaign_name, $campaign_table)) {
+              $camp[] = 'Yes';
+            } elseif (!in_array($campaign_data[0]->campaign_name, $campaign_table)) {
+              $camp[] = 'No';
+            }
 
-			// print_r($campaign_data);
-            $this->table->add_row('<input type="checkbox" class="flat" id="table_records" name="table_records[]" value="' . $get_user->id . '"><input type="hidden" id="row_sort_order" name="row_sort_order[]" value="' . $get_user->id . '">', $get_user->name, $get_user->email, $get_user->visited_date, $cell);
+            $row = array(
+              '<input type="checkbox" class="flat" id="table_records" name="table_records[]" value="' . $get_user->id . '"><input type="hidden" id="row_sort_order" name="row_sort_order[]" value="' . $get_user->id . '">',
+              $get_user->name,
+              $get_user->email,
+              $get_user->visited_date
+            );
+
+            $row = array_merge($row, $camp);
+            $row = array_merge($row, $cell);
+
+            $this->table->add_row($row);
         }       
-        
-        die;
+      
 	
         // Table open
         
@@ -99,9 +113,7 @@ class Email_blast extends MX_Controller
 		$this->table->set_template($template);
         
 		// Table heading row
-    foreach($campaigns as $campaign):        
-      $campaign_table[] =	$campaign->campaign_name;
-    endforeach;
+    
 		//$campaign = implode(",",$campaign_table);
 		
 		$heading = array('<input type="checkbox" id="check-all" class="flat">', 'Name', 'Email', 'Visited Date');
