541ef2105eae2fdfe45081964ce93ba09093da52
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2019-08-06 16:14:17 +0530

    update email blast

--- admin/application/modules/email_blast/controllers/Email_blast.php
+++ admin/application/modules/email_blast/controllers/Email_blast.php
@@ -79,14 +79,7 @@ class Email_blast extends MX_Controller
                 $txgidocs = 'NO';
             }
 
-            if (!empty($email_track_data) && $email_track_data[0]->google === '1') {
-                $google = 'YES';
-            } else {
-                $google = 'NO';
-            }
-      
-        
-            $this->table->add_row('<input type="checkbox" class="flat" id="table_records" name="table_records[]" value="' . $get_user->id . '"><input type="hidden" id="row_sort_order" name="row_sort_order[]" value="' . $get_user->id . '">', $get_user->name, $get_user->email, $get_user->visited_date, $txgidocs, $google, $facebook, $cell);
+            $this->table->add_row('<input type="checkbox" class="flat" id="table_records" name="table_records[]" value="' . $get_user->id . '"><input type="hidden" id="row_sort_order" name="row_sort_order[]" value="' . $get_user->id . '">', $get_user->name, $get_user->email, $get_user->visited_date, $cell);
         }
         $campaigns = $this->Email_blast_model->get_campaign_name();
 		
