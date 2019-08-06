ce8dfe5d973f8cc0c86cdbd45bf061fe4b7c2770
saravana <31875990+saravanasana@users.noreply.github.com>
2019-08-06 17:56:33 +0530

    Update Email_blast.php

--- admin/application/modules/email_blast/controllers/Email_blast.php
+++ admin/application/modules/email_blast/controllers/Email_blast.php
@@ -75,11 +75,13 @@ class Email_blast extends MX_Controller
 
             echo '<pre>';
             print_r($campaign_data);
-            die;
+            
 
 			// print_r($campaign_data);
             $this->table->add_row('<input type="checkbox" class="flat" id="table_records" name="table_records[]" value="' . $get_user->id . '"><input type="hidden" id="row_sort_order" name="row_sort_order[]" value="' . $get_user->id . '">', $get_user->name, $get_user->email, $get_user->visited_date, $cell);
-        }        	
+        }       
+        
+        die;
 	
         // Table open
         
