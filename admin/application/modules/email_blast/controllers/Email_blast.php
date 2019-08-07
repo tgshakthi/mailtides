877c54d2d833f3848e188240c95d902e34606f10
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2019-08-07 12:49:43 +0530

    update email blast

--- admin/application/modules/email_blast/controllers/Email_blast.php
+++ admin/application/modules/email_blast/controllers/Email_blast.php
@@ -73,13 +73,17 @@ class Email_blast extends MX_Controller
 		  $campaign_users = $this->Email_blast_model->get_campaign_data($get_user->id);
 		  foreach($campaign_users as $campaign_user):
 			$campaign_user_name = $campaign_user->campaign_name;
-			$campaign_name[] = $campaign_user_name;
+			if(!empty($campaign_user_name)):
+				$campaign_name[] = '<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>';
+			else:
+				$campaign_name[] = '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>';
+			endif;
+			
 		  endforeach;
 		 
 		  $heading_data = array('<input type="checkbox" class="flat" id="table_records" name="table_records[]" value="' . $get_user->id . '"><input type="hidden" id="row_sort_order" name="row_sort_order[]" value="' . $get_user->id . '">', $get_user->name, $get_user->email, $get_user->visited_date);
 		  $heading_data = array_merge($heading_data,$campaign_name);
 		  $heading_data = array_merge($heading_data,array($cell));
-		  print_r($heading_data);
          
 		  $this->table->add_row($heading_data);
       }
