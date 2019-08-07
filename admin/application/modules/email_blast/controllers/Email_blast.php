60154c87c76a671cdb5ff85d301e0012f4054f3d
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2019-08-07 13:19:44 +0530

    update email blast

--- admin/application/modules/email_blast/controllers/Email_blast.php
+++ admin/application/modules/email_blast/controllers/Email_blast.php
@@ -74,7 +74,7 @@ class Email_blast extends MX_Controller
 		  foreach($campaign_users as $campaign_user):
 			$campaign_user_name = $campaign_user->campaign_users;
 			print_r(explode(",",$campaign_user_name));
-			print_r($get_user->id);
+			print_r(array($get_user->id));
 			if (in_array(explode(",",$campaign_user_name),array($get_user->id))):
 				echo 'test';die;
 				$campaign_name[] = '<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>';
