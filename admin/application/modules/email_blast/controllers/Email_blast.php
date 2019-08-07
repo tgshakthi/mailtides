9623464c54b13c9d737ecd7e2a88edda813771d6
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2019-08-07 12:17:18 +0530

    update email blast

--- admin/application/modules/email_blast/controllers/Email_blast.php
+++ admin/application/modules/email_blast/controllers/Email_blast.php
@@ -71,11 +71,12 @@ class Email_blast extends MX_Controller
           );
 		  
 		  $campaign_users = $this->Email_blast_model->get_campaign_data($get_user->id);
-		  print_r($campaign_users);die;
-		  if(in_array($campaign_users[0]->campaign_name)):		  
-			echo 'test';
+		  foreach($campaign_users as $campaign_user):
+			print_r($campaign_user);die;
 		  endif;
 
+		  endforeach;
+		  
           $this->table->add_row('<input type="checkbox" class="flat" id="table_records" name="table_records[]" value="' . $get_user->id . '"><input type="hidden" id="row_sort_order" name="row_sort_order[]" value="' . $get_user->id . '">', $get_user->name, $get_user->email, $get_user->visited_date, $cell);
       }
       
