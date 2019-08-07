e69af60ac3a9201e0d511f43c4ce958cce86fa68
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2019-08-07 12:10:35 +0530

    update email blast

--- admin/application/modules/email_blast/controllers/Email_blast.php
+++ admin/application/modules/email_blast/controllers/Email_blast.php
@@ -70,8 +70,8 @@ class Email_blast extends MX_Controller
             'data' => $anchor_delete
           );
 		  
-		  $campaign-users = $this->Email_blast_model->get_campaign_data($get_user->id);
-		  print_r($campaign-users);die;
+		  $campaign_users = $this->Email_blast_model->get_campaign_data($get_user->id);
+		  print_r($campaign_users);die;
 
           $this->table->add_row('<input type="checkbox" class="flat" id="table_records" name="table_records[]" value="' . $get_user->id . '"><input type="hidden" id="row_sort_order" name="row_sort_order[]" value="' . $get_user->id . '">', $get_user->name, $get_user->email, $get_user->visited_date, $cell);
       }
