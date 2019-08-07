17d8f46dccc8347a5c56a66228ec5890715788f3
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2019-08-07 12:23:14 +0530

    update email blast

--- admin/application/modules/email_blast/controllers/Email_blast.php
+++ admin/application/modules/email_blast/controllers/Email_blast.php
@@ -72,11 +72,15 @@ class Email_blast extends MX_Controller
 		  
 		  $campaign_users = $this->Email_blast_model->get_campaign_data($get_user->id);
 		  foreach($campaign_users as $campaign_user):
-			print_r($campaign_user->campaign_name);die;
-		  
+			$campaign_user_name = $campaign_user->campaign_name;
+			$campaign_name[] = $campaign_user_name;
 		  endforeach;
 		  
-          $this->table->add_row('<input type="checkbox" class="flat" id="table_records" name="table_records[]" value="' . $get_user->id . '"><input type="hidden" id="row_sort_order" name="row_sort_order[]" value="' . $get_user->id . '">', $get_user->name, $get_user->email, $get_user->visited_date, $cell);
+		  $heading_data = array('<input type="checkbox" class="flat" id="table_records" name="table_records[]" value="' . $get_user->id . '"><input type="hidden" id="row_sort_order" name="row_sort_order[]" value="' . $get_user->id . '">', $get_user->name, $get_user->email, $get_user->visited_date);
+		  $heading_data = array_merge($heading_data,$campaign_name)
+		  $heading_data = array_merge($heading_data,$cell)
+          
+		  $this->table->add_row($heading_data);
       }
       
 	  foreach($campaign_name_datas as $campaign_name_data):
