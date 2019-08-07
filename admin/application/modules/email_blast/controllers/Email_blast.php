e67c3ce2f2afed2b18410dc1c71c4c41b48b9661
karthika19041991 <43335051+karthika19041991@users.noreply.github.com>
2019-08-07 15:50:30 +0530

    Update Email_blast.php

--- admin/application/modules/email_blast/controllers/Email_blast.php
+++ admin/application/modules/email_blast/controllers/Email_blast.php
@@ -618,9 +618,7 @@ class Email_blast extends MX_Controller
       // Send Email    
     function send_email_blast()
     {
-      echo"<pre>";
-      print_r($_POST);
-      die;
+      
        
         $website_id          = $this->admin_header->website_id();
         $mail_configurations = $this->Email_blast_model->get_mail_configuration($website_id);
@@ -659,7 +657,9 @@ class Email_blast extends MX_Controller
 			
             $get_template_id = $this->Email_blast_model->get_campaign_template($campaign_id);
             $template_id= $get_template_id[0]->template_id;             
-           
+            echo"<pre>";
+            print_r($get_template_id);
+            die;
             foreach ($campaign_users as $get_users) :
               $get_user = $this->Email_blast_model->get_users_by_id($get_users);
               // if ($get_user->status == '1'):
