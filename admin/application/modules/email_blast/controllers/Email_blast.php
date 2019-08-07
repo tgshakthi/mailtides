d8b4dad9708d9e0a7d193bb25bde38e3c4acab7c
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2019-08-07 15:06:31 +0530

    update email blast

--- admin/application/modules/email_blast/controllers/Email_blast.php
+++ admin/application/modules/email_blast/controllers/Email_blast.php
@@ -71,9 +71,7 @@ class Email_blast extends MX_Controller
           );
 		  
 		  $campaign_users = $this->Email_blast_model->get_campaign_data($get_user->id);
-		 // echo '<pre>';
-		 print_r($get_user->id);
-		  print_r($campaign_users);
+		 
 		  foreach($campaign_users as $campaign_user):
 			$campaign_user_name = $campaign_user->campaign_users;
 			$campaign_array = explode(",",$campaign_user_name);
