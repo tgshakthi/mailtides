043cc62393335368538c993d1363a45021f7ff2f
karthika19041991 <43335051+karthika19041991@users.noreply.github.com>
2019-08-09 17:01:19 +0530

    updated
    $get_campaign_name

--- admin/application/modules/email_blast/controllers/Email_blast.php
+++ admin/application/modules/email_blast/controllers/Email_blast.php
@@ -1973,24 +1973,13 @@ class Email_blast extends MX_Controller
   
     $campaign_type_id=$this->input->post('campaign_type_id');
     $get_campaign_names=$this->Email_blast_model->get_campaign_name__Bi_reports();
-     foreach($get_campaign_names as $get_campaign_name):
-      
-     if($campaign_type_id==$get_campaign_name->campaign_type):
-          $campaign_name []= $get_campaign_name->campaign_name;
-              $get_email_track = $this->Email_blast_model->select_campaign_user($campaign_type_id);
-    if(!empty($get_email_track)):
-          $users=explode(",",$get_email_track[0]->campaign_users);
-        for($i=0;$i<count($users);$i++):
-         $get_campaign_users=$this->Email_blast_model->get_campaign_users_by_campaign_type($users[$i]);
-          $campaign_users[]= $get_campaign_users[0]->name;
-        endfor;
-      endif;
-        endif;
-     endforeach;
+      foreach($get_campaign_names as $get_campaign_name):
+        $campaign_name[]=$get_campaign_name->campaign_name;
+      endforeach;
      
       $data['campaign']= $campaign_name;
-       $data['campaign_name_count']=count( $campaign_name);
-      $data['users'] = count($campaign_users); 
+      //  $data['campaign_name_count']=count( $campaign_name);
+      // $data['users'] = count($campaign_users); 
      echo json_encode($data);
     
    }
