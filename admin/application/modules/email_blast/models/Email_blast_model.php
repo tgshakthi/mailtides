3b5d395857dc9c2f2054f4b4082f6ab532d93542
karthika19041991 <43335051+karthika19041991@users.noreply.github.com>
2019-08-09 15:33:21 +0530

    Update Email_blast_model.php

--- admin/application/modules/email_blast/models/Email_blast_model.php
+++ admin/application/modules/email_blast/models/Email_blast_model.php
@@ -705,6 +705,11 @@ class Email_blast_model extends CI_Model
                  'is_deleted'=>'0'
              ));
              $query   = $this->db->get($this->table_campaign);
+             $records = array();
+             if ($query->num_rows() > 0):
+                 $records = $query->result();
+             endif;
+             return $records;
          }
 	 // Get Campaign By Id
     function get_campaign_type_by_id($id)
