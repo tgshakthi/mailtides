f07d4ebcb910cebc1c2a38c728c6e8124a30c6fb
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2019-09-26 15:50:58 +0530

    sms gateway

--- admin/application/modules/smsgateway/controllers/Smsgateway.php
+++ admin/application/modules/smsgateway/controllers/Smsgateway.php
@@ -22,4 +22,28 @@ class Smsgateway extends MX_Controller
         $this->load->view('template/footer_content');
         $this->load->view('template/footer');
 	}
+	
+	function sms_gateway_data()
+	{
+		// Update the path below to your autoload.php,
+		// see https://getcomposer.org/doc/01-basic-usage.md
+		require_once("application/third_party/Twilio/autoload.php");
+		use Twilio\Rest\Client;
+
+		// Find your Account Sid and Auth Token at twilio.com/console
+		// DANGER! This is insecure. See http://twil.io/secure
+		$sid    = "AC839320f02176c877d19a2816218a9674";
+		$token  = "943086168eb029d1f2e5af5455284fde";
+		$twilio = new Client($sid, $token);
+
+		$message = $twilio->messages
+						  ->create("+17139339132", // to
+								   array(
+									   "body" => "Hello Desss !!!",
+									   "from" => "+12818843247"
+								   )
+						  );
+
+		print($message->sid);
+	}
 }
--- admin/application/modules/smsgateway/views/view.php
+++ admin/application/modules/smsgateway/views/view.php
@@ -1,3 +1,90 @@
+<!-- page content -->
+<div class="right_col" role="main" id="datatable_content">
+  <div class="">
+
+    <div class="page-title">
+      <div class="title_left">
+        <?php echo heading($heading, '3');?>
+      </div>
+    </div>
+
+    <div class="clearfix"></div>
+
+    <div class="row">
+	  <?php if ($this->session->flashdata('success') != '') : // Display session data ?>
+        <div class="alert alert-success alert-dismissible fade in text-center" id="success-alert" role="alert">
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span>
+          </button>
+          <strong>Success!</strong> <?php echo $this->session->flashdata('success');?>
+        </div>
+      <?php endif; ?>
+
+      <?php if ($this->session->flashdata('error') != '') : // Display session data ?>
+        <div class="alert alert-warning alert-dismissible fade in text-center" id="warning-alert" role="alert">
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span>
+          </button>
+          <strong><?php echo $this->session->flashdata('error');?></strong>
+        </div>
+      <?php endif; ?>
+
+		<div class="col-md-12 col-xs-12">
+				<div class="x_panel">
+
+					<div class="x_title">
+
+						<?php
+							echo heading('Vertical Tab Title', '2');
+
+							$attributes = array('class' => 'nav navbar-right panel_toolbox');
+							$list = array('<a class="collapse-link"><i class="fa fa-chevron-up"></i></a>');
+							echo ul($list,$attributes);
+						?>
+
+						<div class="clearfix"></div>
+					</div>
+
+					<div class="x_content">
+
+						<?php
+							// Break Tag
+							echo br();
+
+							// Form Tag
+							  echo form_open(
+								'smsgateway/sms_gateway_data',
+								'class="form-horizontal form-label-left" id="demo-form2" data-parsley-validate'
+							  );
+						?>
+						<div class="ln_solid"></div>
+						<div class="col-md-12 col-sm-12 col-xs-12">
+							<div class="input-button-group">
+								<?php
+									// Submit Button
+									
+									echo form_submit(
+										array(
+											'class' => 'btn btn-success',
+											'id'    => 'btn',
+											'value' => 'Submit'
+										)
+									);
+								?>
+							</div>
+						</div>
+
+						<?php
+							//Form close
+							echo form_close();
+						?>
+
+					</div>
+				</div>
+			</div>
+    </div>
+  </div>
+</div>
+<!-- /page content -->
+
 <?php
 
 // Update the path below to your autoload.php,
