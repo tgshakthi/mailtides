7935215816f01a4c4e7cf63c02f717c921daee97
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2019-09-27 17:40:54 +0530

    Update Email_blast.php

--- admin/application/modules/email_blast/controllers/Email_blast.php
+++ admin/application/modules/email_blast/controllers/Email_blast.php
@@ -2033,6 +2033,7 @@ class Email_blast extends MX_Controller
 		$sid    = "AC839320f02176c877d19a2816218a9674";
 		$token  = "943086168eb029d1f2e5af5455284fde";
 		$twilio = new Client($sid, $token);
+		
 		/* $patient_phone_nos = $this->Email_blast_model->get_patient_phone_numbers();		
 		if(!empty($patient_phone_nos)):
 			foreach($patient_phone_nos as $patient_phone_no):				
@@ -2054,9 +2055,31 @@ Thank You'										,
 				endif;
 			endforeach;
 		endif; */
-		// print($message->sid);
-		// echo '<br>';
-		// print($message->status);
+		
+		$numbers = array(
+						'+17139339132',
+						'+17135578001',
+						'+18324449173',
+						'+12813947218'
+					);
+		foreach($numbers as $number)
+		{
+			$message = $twilio->messages
+							->create($number, // to
+										array(
+										   "body" => "Dear Chandler,
+Thanks for visiting the Digestive & Liver Disease Consultants, P.A . Your wellbeing is very important to us. To help serve you and others more effectively, please take a moment to let us know about your experience.	
+Please click the link below and give your feedback. 
+https://tinyurl.com/yy98b7u3
+Thank You"										,
+										   "from" => "+12818843247"
+										)
+									);
+		}
+		
+		print($message->sid);
+		echo '<br>';
+		print($message->status);
 	}
 
     
