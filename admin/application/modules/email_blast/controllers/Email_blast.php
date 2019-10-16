7d5408158f4fc06870b03d0b1242ead6e2c031d9
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2019-10-16 18:38:46 +0530

    Update Email_blast.php

--- admin/application/modules/email_blast/controllers/Email_blast.php
+++ admin/application/modules/email_blast/controllers/Email_blast.php
@@ -2067,21 +2067,37 @@ class Email_blast extends MX_Controller
 		print($message->sid); */
 		
 		$website_id = $this->admin_header->website_id();
+		
         // Replace key value with your own api key
-        /* $url = "https://api.data247.com/v3.0?key=262385da4166dc1dc5&api=MT&phone=+17135578001";
+       /*  $url = "https://api.data247.com/v3.0?key=262385da4166dc1dc5&api=MT&phone=+17135578001";
         $result = @file_get_contents($url);
         if ($result){
             $result = @json_decode($result, true);
             if (!empty($result['response']['status']) && $result['response']['status'] == 'OK'){				
 				$sms_address = $result['response']['results'][0]['sms_address'];
-				
-				
 				print_r($sms_address);die;
             }
-        }	 */	
+        } */
 		
-		$mail_config = $this->Email_blast_model->get_mail_configuration($website_id);
-		print_r($mail_config);
+		require_once "vendor/autoload.php"; //PHPMailer Object
+		$mail = new PHPMailer; //From email address and name 
+		$mail->From = "velusamy@desss.com"; 
+		$mail->FromName = "Info Desss"; //To address and name 
+		$mail->addAddress("velusamy@desss.com");//Recipient name is optional
+		// $mail->addAddress("recepient1@example.com"); //Address to which recipient will reply 
+		// $mail->addReplyTo("reply@yourdomain.com", "Reply"); //CC and BCC 
+		// $mail->addCC("cc@example.com"); 
+		$mail->addBCC("velusamy@desss.com"); //Send HTML or Plain Text email 
+		$mail->isHTML(true); 
+		$mail->Subject = "Send Sms"; 
+		$mail->Body = "<i>Welcome Desss!!!</i>";
+		$mail->AltBody = "This is the plain text version of the email content"; 
+		if(!$mail->send()) 
+		{
+		echo "Mailer Error: " . $mail->ErrorInfo; 
+		} 
+		else { echo "Message has been sent successfully"; 
+		}		
 		// redirect('email_blast');
 	}
 
