fc289336dd760fb5aea07ce188654e5aa64d2e39
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-01-23 20:40:55 +0530

    Update Email_sms_blast.php

--- admin/application/modules/email_sms_blast/controllers/Email_sms_blast.php
+++ admin/application/modules/email_sms_blast/controllers/Email_sms_blast.php
@@ -995,13 +995,14 @@ class Email_sms_blast extends MX_Controller
 			$mail->SMTPKeepAlive = true;
 			$mail->Host = 'smtp.gmail.com';
 			$mail->Port = 587;
-			$mail->IsHTML(true); 
+			$mail->IsHTML(true);
+			$mail->SetFrom("example@gmail.com");
+			$mail->FromName = 'Digestive & Liver Disease Consultants , P.A';			
 			$mail->Username = "desssinfotest@gmail.com";
 			$mail->Password = "Houston77042";
 			$mail->SMTPAuth = true;			
 			$mail->IsHTML(true);
-			$mail->SetFrom("example@gmail.com");
-			$mail->FromName = 'Digestive & Liver Disease Consultants , P.A';
+			
 			$mail->Subject = "";
 			
 			if($provider_name == 'dldc'):							 
