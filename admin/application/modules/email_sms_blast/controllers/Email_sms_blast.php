8f5f480fdf1dddfa65273b12490108d99662f05b
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-01-23 20:28:35 +0530

    Update Email_sms_blast.php

--- admin/application/modules/email_sms_blast/controllers/Email_sms_blast.php
+++ admin/application/modules/email_sms_blast/controllers/Email_sms_blast.php
@@ -1003,7 +1003,7 @@ class Email_sms_blast extends MX_Controller
 			$mail->FromName = 'Digestive & Liver Disease Consultants , P.A';
 			$mail->IsHTML(true);
 			$mail->Subject = "";
-			echo '<pre>';print_r($mail);die;
+			// echo '<pre>';print_r($mail);die;
 			if($provider_name == 'dldc'):							 
 				//Others DLDC
 				$tiny_url = 'tinyurl.com/vj4mjvg';
