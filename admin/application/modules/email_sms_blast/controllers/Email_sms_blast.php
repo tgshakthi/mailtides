c7d9a5b7b851c5740798fd4d110798c4c69eb000
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-01-23 13:04:26 +0530

    Update Email_sms_blast.php

--- admin/application/modules/email_sms_blast/controllers/Email_sms_blast.php
+++ admin/application/modules/email_sms_blast/controllers/Email_sms_blast.php
@@ -991,29 +991,24 @@ class Email_sms_blast extends MX_Controller
   
 		if(!empty($sms_address)):
 			$mail_config = $this->Email_sms_blast_model->get_mail_configuration($website_id );
-			require_once "application/third_party/PHPMailer/vendor/autoload.php"; //PHPMailer Object
+			require_once "application/third_party/PHPMailer/vendor/autoload.php"; //PHPMailer Object			
 			
 			$mail = new PHPMailer;
-
 			//$mail->SMTPDebug = 3;                               // Enable verbose debug output
-
 			$mail->isSMTP();                                      // Set mailer to use SMTP
 			$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+			$mail->Port = 587;  // TCP port to connect to
+			$mail->SMTPSecure = 'tls';    
 			$mail->SMTPAuth = true;                               // Enable SMTP authentication
 			$mail->Username = 'desssinfotest@gmail.com';                 // SMTP username
 			$mail->Password = 'Houston77042';                           // SMTP password
-			//$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-			$mail->Port = 25;                                    // TCP port to connect to
-
+			 
 			$mail->setFrom('desssinfotest@gmail.com', 'Mailer');
 			$mail->addAddress('7135578001@vtext.com', 'Chandler');     // Add a recipient
 			$mail->addBCC('velusamy@desss.com');
-
 			$mail->isHTML(true);                                  // Set email format to HTML
-
 			$mail->Subject = 'Here is the subject';
-			$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-			$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+			$mail->Body    = 'This is the HTML message body in bold!';
 
 			if(!$mail->send()) {
 				echo 'Message could not be sent.';
