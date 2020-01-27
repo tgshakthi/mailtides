8ac6bb520610490d5faaae08f7062b8f67a600c3
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-01-27 17:22:04 +0530

    Update mail.php

--- admin/application/modules/email_sms_blast/views/mail.php
+++ admin/application/modules/email_sms_blast/views/mail.php
@@ -51,17 +51,17 @@
 		<div class="x_content">
 			<div class="page_buut_right">
 				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-					<a href="<?php echo base_url();?>email_sms_blast/send_email_blast_status" class="btn btn-primary">
+					<a href="<?php echo base_url();?>email_sms_blast/send_email_blast_status" class="btn btn-danger">
 						<i class="fa fa-envelope"></i> Send Email
 					</a>
 				</div>
 				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-					<a href="<?php echo base_url();?>email_sms_blast/send_fb_email_blast" class="btn btn-primary">
+					<a href="<?php echo base_url();?>email_sms_blast/send_fb_email_blast" class="btn btn-danger">
 						<i class="fa fa-envelope"></i> Send Fb Email
 					</a>
 				</div>
 				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-					<a href="<?php echo base_url();?>email_sms_blast/send_txgidocs_email_blast" class="btn btn-primary">
+					<a href="<?php echo base_url();?>email_sms_blast/send_txgidocs_email_blast" class="btn btn-danger">
 						<i class="fa fa-envelope"></i> Send Txgidocs Email
 					</a>
 				</div>
