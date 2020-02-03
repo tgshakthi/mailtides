814e855c6cd49214b0762c6cda37b0a9cb02e17f
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-02-03 17:24:57 +0530

    Update email-sms-blasts.js

--- admin/assets/email-blast/js/email-sms-blasts.js
+++ admin/assets/email-blast/js/email-sms-blasts.js
@@ -386,52 +386,34 @@ $(document).ready(function () {
 	
 	// Email Tracking Datatable Filter
 	if ($('#datatable-email').length) {
-		// Datatable - One ( Master Campaign Datepicker Filter)
-		$.fn.dataTable.ext.search.push(function (settings, data, dataIndex) {
-			var min = $('#min').datepicker('getDate');
-			var max = $('#max').datepicker('getDate');
-			var startDate = new Date(data[3]);
-			if (min == null && max == null) {
-				return true;
-			}
-			if (min == null && startDate <= max) {
-				return true;
-			}
-			if (max == null && startDate >= min) {
-				return true;
-			}
-			if (startDate <= max && startDate >= min) {
-				return true;
-			}
-			return false;
-		});
-
 		var table = $('#datatable-email').DataTable({
-			"pageLength": 200
+			pageLength: 200,
+			dom: 'Bfrtip',
+			orderCellsTop: true,
+			responsive: !0
 		});
 
+		// Clone Previous Row for filter input
 		$('#datatable-email>thead>tr')
 			.clone(true)
 			.appendTo('#datatable-email thead');
 		$('#datatable-email>thead>tr:eq(1)>th').each(function (i) {
-			// var title = $(this).text();
-			// if (title.length > 0 && title != 'S.No') {
-				// $('input', this).on('keyup change', function () {
+			var title = $(this).text();
+			if (title.length > 0) {
+				$(this).html(
+					'<input type="text" placeholder="Search ' + title + '" />'
+				);
+				$('input', this).on('keyup change', function () {
 					if (table.column(i).search() !== this.value) {
 						table
 							.column(i)
 							.search(this.value)
-							.sort()
 							.draw();
 					}
-				// });
-			// }
+				});
+			}
 		});
 
-		// Event listener to the two range filtering inputs to redraw on input
-		$('#min, #max').change(function () {
-			table.draw();
-		});
 	}
 
 // SMS Tracking Datatable Filter
