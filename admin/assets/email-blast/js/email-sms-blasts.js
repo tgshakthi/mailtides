3b61bd48627066faedacc3f3626ee1e90b3412ab
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-02-03 18:36:32 +0530

    Update email-sms-blasts.js

--- admin/assets/email-blast/js/email-sms-blasts.js
+++ admin/assets/email-blast/js/email-sms-blasts.js
@@ -411,8 +411,8 @@ $(document).ready(function () {
 		});
 
 		$('#datatable-email>thead>tr')
-			.clone(true)
-			.appendTo('#datatable-email thead');
+			.clone(true);
+			//.appendTo('#datatable-email thead');
 		$('#datatable-email>thead>tr:eq(1)>th').each(function (i) {
 			var title = $(this).text();
 			if (title.length > 0 && title != 'S.No') {
