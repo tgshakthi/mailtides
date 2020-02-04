a5ac2e05b0c78ad20a8dc7f34323cc82dc7963f0
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-02-04 16:34:13 +0530

    Update graphical_reports.php

--- admin/application/modules/email_sms_blast/views/graphical_reports.php
+++ admin/application/modules/email_sms_blast/views/graphical_reports.php
@@ -53,7 +53,7 @@
               </div>
             </div>
           </div>
-		  <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">							
+			<!-- <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">							
 				<div class="email-blast-date">
 			
 					<div>
@@ -65,7 +65,7 @@
 						<input type="text" id="max">
 					</div>
 				</div>
-			</div>
+			</div> -->
         </div>
       </div>
       <div class="col-md-12 col-sm-12 col-xs-12">
