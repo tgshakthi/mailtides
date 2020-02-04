e00a9d62ff792dac1c1cc6d974a1cc4efb4b8bb2
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-02-04 15:19:40 +0530

    Update graphical_reports.php

--- admin/application/modules/email_sms_blast/views/graphical_reports.php
+++ admin/application/modules/email_sms_blast/views/graphical_reports.php
@@ -53,7 +53,7 @@
               </div>
             </div>
           </div>
-		  <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">							
+		  <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">							
 				<div class="email-blast-date">
 			
 					<div>
