f01cbbc96560f70e2d61f2796c081f692c11e13d
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-02-04 16:15:17 +0530

    Update Email_link_open.php

--- admin/application/modules/email_link_open/controllers/Email_link_open.php
+++ admin/application/modules/email_link_open/controllers/Email_link_open.php
@@ -114,8 +114,8 @@ class Email_link_open extends MX_Controller
 			//DLDC SMS Link
 			$campaign_tiny_urls = $this->Email_link_open_model->get_campaign_category('10');
 			$this->update_dldc_sms_feedback($user_id);			
-			//redirect('https://tinyurl.com/rl8opbq');
-			redirect($campaign_tiny_urls[0]['tiny_url']);
+			redirect('https://tinyurl.com/rl8opbq?tiny_url=dldc_reviews');
+			//redirect($campaign_tiny_urls[0]['tiny_url']);
 		}
 	}
 	
