1baf4204c8a21395f11c8c04e45d1450194ca8ca
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-02-07 12:35:28 +0530

    Update editor.js

--- admin/assets/email-blast/js/editor.js
+++ admin/assets/email-blast/js/editor.js
@@ -2493,7 +2493,7 @@
 		var $button = $(this),
 			data = $("#mail-template").html();
 			data = data.replace(/(<button.*?>.*?<\/button>)/g,'');
-		
+		alert($button);
 		$button.tooltip('hide');
 		
 		$button.prop('disabled',true);
