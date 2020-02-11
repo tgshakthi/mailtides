583604fe2faafdd08bbe09de4e8f93274bf65e9d
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-02-11 16:53:26 +0530

    Update Email_blasts.php

--- admin/application/modules/email_blasts/controllers/Email_blasts.php
+++ admin/application/modules/email_blasts/controllers/Email_blasts.php
@@ -3323,59 +3323,8 @@ class Email_blasts extends MX_Controller
 	
 	function test_email()
 	{
-		$RDmax = $this->input->post('RD');
-		$IRmax = $this->input->post('IR');
-		$REmax = $this->input->post('RE');
 		echo '<pre>';
 		print_r($_POST);
-		
-		if(array("table[data-edit]")){
-			$RD = "table[data-edit]";
-		}else{
-			$RD = array();
-		}
-		$IR = array("img");
-		$RE = array("#dd-sidebar-left,#dd-sidebar-right");
-		
-		for($i=0; $i < $RDmax; $i++)
-		{
-			print_r($RD[$i]);
-			 /* ($RD[$i]).css({
-				width : '100%'
-			});
-			($RD[$i]).find('tr > td').css({
-				padding:'',
-				margin:''
-			});
-			
-			($RD[$i]).find('table tr > td').css({
-				padding:'',
-				margin:''
-			}); */
-		}
-		
-		for($j=0; $j < $IRmax; $j++)
-		{
-			echo 'test2';
-			/* ($IR[$j]).css({
-				width : '100%',
-				height : 'auto'
-			})
-			.removeAttr('class'); */
-		}
-		
-		for($r=0; $r < $REmax; $r++)
-		{
-			echo 'test3';
-			/* $rem = $($RE[$r]).html().trim();
-			if($rem == '')
-				($RE[$r]).remove();
-			else{
-				($RE[$r]).find('a').each(function(){
-					$(this).css('text-decoration','none');
-				});
-			} */
-		}
 		die;
 	}
 }
