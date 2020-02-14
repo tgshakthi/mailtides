fc1547f1266fa39174f1c93ef0883aaf3931d9f6
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-02-14 14:00:40 +0530

    Update add_edit_email_template_generate.php

--- admin/application/modules/email_blasts/views/add_edit_email_template_generate.php
+++ admin/application/modules/email_blasts/views/add_edit_email_template_generate.php
@@ -133,11 +133,11 @@
          </div>
       </div>
       <div id="modal" class="reset-this"></div>
-      <button class="btn btn-lg preview-green btn-materialize btn-left-bottom btn-left-bottom-1 hidden" type="button"
+      <button class="btn btn-lg preview-green btn-materialize btn-left-bottom btn-left-bottom-1" type="button"
          id="preview" title="Preview" data-toggle="tooltip" data-placement="left" data-trigger="hover"><span
          class="glyphicon glyphicon-zoom-in"></span></button>
       
-      <button class="btn btn-lg setting-red btn-materialize btn-left-bottom btn-left-bottom-3 hidden" type="button"
+      <button class="btn btn-lg setting-red btn-materialize btn-left-bottom btn-left-bottom-2" type="button"
          id="setting" title="Layout Options" data-toggle="tooltip" data-placement="left" data-trigger="hover"><span
          class="fa fa-cog fa-spin"></span></button>
       <div id="alerts"></div>
