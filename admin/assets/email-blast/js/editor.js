bda77d7b30595d7774b822806e7441b548367c25
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-02-15 11:05:37 +0530

    Update editor.js

--- admin/assets/email-blast/js/editor.js
+++ admin/assets/email-blast/js/editor.js
@@ -2438,6 +2438,6 @@ $("#test").on('click',function(e){
 	});
 });
 $( document ).ready(function() {
-	// alert('');
-    localStorage.removeItem(storage);
+	alert('');
+    localStorage.clear();
 });
