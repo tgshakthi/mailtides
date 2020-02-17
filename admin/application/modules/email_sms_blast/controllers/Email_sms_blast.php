6f809ee4fb442b64a599d4b99513bd2c40cc6226
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-02-17 18:46:28 +0530

    Update Email_sms_blast.php

--- admin/application/modules/email_sms_blast/controllers/Email_sms_blast.php
+++ admin/application/modules/email_sms_blast/controllers/Email_sms_blast.php
@@ -3923,6 +3923,6 @@ class Email_sms_blast extends MX_Controller
 	
 	function import_send_email_sms_filter_data()
 	{
-		echo '<pre>';print_r($_POST);die;
+		echo '<pre>';echo 'test';print_r($_POST);die;
 	}
 }
