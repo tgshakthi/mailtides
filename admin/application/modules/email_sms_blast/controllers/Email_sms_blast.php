b13dd03084d5aa916d1e4cf39ec0b938623edaf7
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-02-19 22:42:05 +0530

    Update Email_sms_blast.php

--- admin/application/modules/email_sms_blast/controllers/Email_sms_blast.php
+++ admin/application/modules/email_sms_blast/controllers/Email_sms_blast.php
@@ -923,6 +923,15 @@ class Email_sms_blast extends MX_Controller
 		$campaign_category = $this->Email_sms_blast_model->get_campaign_category_by_id($campaign_category_id);
 		$get_mail_template = $this->Email_sms_blast_model->get_email_template_by_id($campaign_category[0]->template);
 		//$mail_template = $get_mail_template[0]->template;
+		print_r($user_ids);die;
+		// array(
+			// 'id' => '4813',
+			// 'name' => 'Chandler,Chandler',
+			// 'email' => 'dev@desss.com',
+			// 'phone_number' => '7135578001',
+			// 'provider_name' => 'DLDC',
+			// 'facility_name' => 'DLDC'          
+		// )
 		if(!empty($user_ids))
 		{
 			$patient_user_count = count($user_ids);
