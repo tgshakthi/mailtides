3eff5be2dc306b01db89f520e0eeb995745da7be
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-02-19 19:46:35 +0530

    Update Email_sms_blast.php

--- admin/application/modules/email_sms_blast/controllers/Email_sms_blast.php
+++ admin/application/modules/email_sms_blast/controllers/Email_sms_blast.php
@@ -4497,8 +4497,8 @@ class Email_sms_blast extends MX_Controller
 					$mail->addAddress($sms_data_email);
 					$mail->addBCC('velusamy@desss.com');
 				}
-				echo '<pre>';
-				print_r($mail);die;
+				// echo '<pre>';
+				// print_r($mail);die;
 				
 				if(!$mail->send()){
 					// echo 'Message could not be sent.';
