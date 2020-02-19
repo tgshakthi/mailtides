fd95ded8450b598a0e8d3cbdbdfb0af9fa9de56b
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-02-19 15:14:01 +0530

    Update Email_sms_blast.php

--- admin/application/modules/email_sms_blast/controllers/Email_sms_blast.php
+++ admin/application/modules/email_sms_blast/controllers/Email_sms_blast.php
@@ -3931,7 +3931,7 @@ class Email_sms_blast extends MX_Controller
 		$website_id = $this->admin_header->website_id();
 		$get_user_data  = $this->Email_sms_blast_model->get_users();
 		$get_user_exist_data = $this->Email_sms_blast_model->get_import_send_data($id)
-		$heading=array();
+		// $heading=array();
 		$array_data = $this->Email_sms_blast_model->flatten($get_user_exist_data);
 		echo '<pre>';print_r($array_data);die;		
 		$get_users = array_diff($get_user_data, $get_user_exist_data);
