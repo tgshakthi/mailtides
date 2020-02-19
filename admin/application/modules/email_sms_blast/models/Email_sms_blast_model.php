95c2948cf8c44a4650befbc51f7773a84b4f11aa
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-02-19 15:59:01 +0530

    email sms blast update

--- admin/application/modules/email_sms_blast/models/Email_sms_blast_model.php
+++ admin/application/modules/email_sms_blast/models/Email_sms_blast_model.php
@@ -1166,4 +1166,19 @@ class Email_sms_blast_model extends CI_Model
         endif;
         return $records;
 	}
+	
+	function check_diff_multi($array1, $array2)
+	{
+		$result = array();
+		foreach($array1 as $key => $val) {
+			 if(isset($array2[$key])){
+			   if(is_array($val) && $array2[$key]){
+				   $result[$key] = check_diff_multi($val, $array2[$key]);
+			   }
+		   } else {
+			   $result[$key] = $val;
+		   }
+		}
+		return $result;
+	}
 }
