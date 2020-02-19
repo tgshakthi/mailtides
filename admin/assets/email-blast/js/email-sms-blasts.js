1516bea974e98e845c83ec900bcbf2ddb4ab8505
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-02-19 11:52:52 +0530

    Update email-sms-blasts.js

--- admin/assets/email-blast/js/email-sms-blasts.js
+++ admin/assets/email-blast/js/email-sms-blasts.js
@@ -343,6 +343,7 @@ $(document).ready(function () {
 						campaign_category_id: campaign_category_id
 					},
 					success: function (data) {
+						alert(data);
 						if(data == '1') {
 							alert('Successfully Sent Mail.');
 							document.getElementById('send-email-sms-filter-data-import').disabled = true;
