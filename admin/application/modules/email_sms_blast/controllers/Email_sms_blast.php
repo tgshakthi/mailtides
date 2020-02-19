afc974c18b753766636c9d1ba2f3ab43b81128b1
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-02-19 15:23:41 +0530

    email sms blast update

--- admin/application/modules/email_sms_blast/controllers/Email_sms_blast.php
+++ admin/application/modules/email_sms_blast/controllers/Email_sms_blast.php
@@ -3929,7 +3929,7 @@ class Email_sms_blast extends MX_Controller
 	function get_table_exixts_users($id)
 	{
 		$website_id = $this->admin_header->website_id();
-		$get_user_data  = $this->Email_sms_blast_model->get_users();
+		$get_user_data  = $this->Email_sms_blast_model->get_users_data();
 		echo '<pre>';print_r($get_user_data);die;		
 		$get_user_exist_data = $this->Email_sms_blast_model->get_import_send_data($id);
 		$heading=array();
--- admin/application/modules/email_sms_blast/models/Email_sms_blast_model.php
+++ admin/application/modules/email_sms_blast/models/Email_sms_blast_model.php
@@ -26,6 +26,20 @@ class Email_sms_blast_model extends CI_Model
         endif;
         return $records;      
     }
+	// Get Patient  Users
+    function get_users()
+    {
+        $this->db->select('id');
+        $this->db->where(array(
+            'is_deleted' => '0'
+        ));
+        $query   = $this->db->get($this->table_name);
+        $records = array();
+        if ($query->num_rows() > 0):
+            $records = $query->result();
+        endif;
+        return $records;      
+    }
 	
 	// Get Patient  Users by Id
     function get_users_by_id($user_id)
@@ -1141,7 +1155,7 @@ class Email_sms_blast_model extends CI_Model
 	
 	function get_import_send_data($id)
 	{
-		$this->db->select('*');
+		$this->db->select('user_id');
         $this->db->where(array(
 							'campaign_category_id' => $id
 						 ));
