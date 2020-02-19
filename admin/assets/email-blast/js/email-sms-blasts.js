b35accfd87116347a559df8b7a15586bcbe03662
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-02-19 18:40:16 +0530

    Update email-sms-blasts.js

--- admin/assets/email-blast/js/email-sms-blasts.js
+++ admin/assets/email-blast/js/email-sms-blasts.js
@@ -137,7 +137,7 @@ $(document).ready(function () {
 		$.fn.dataTable.ext.search.push(function (settings, data, dataIndex) {
 			var min = $('#min').datepicker('getDate');
 			var max = $('#max').datepicker('getDate');
-			var startDate = new Date(data[6]);
+			var startDate = new Date(data[4]);
 			if (min == null && max == null) {
 				return true;
 			}
