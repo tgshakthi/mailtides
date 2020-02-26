cf3a810b2ee6b45a27fc28653f15724d46807c2d
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-02-26 16:31:20 +0530

    Update email-sms-blasts.js

--- admin/assets/email-blast/js/email-sms-blasts.js
+++ admin/assets/email-blast/js/email-sms-blasts.js
@@ -1781,8 +1781,7 @@ $(document).ready(function () {
 		    extend: 'collection',
 		    text: 'Export',
 		    buttons: [                    
-		        'excel',
-		        'csv',                    
+		        'excel'                 
 		    ]
 		}],      
 		"ajax": {
