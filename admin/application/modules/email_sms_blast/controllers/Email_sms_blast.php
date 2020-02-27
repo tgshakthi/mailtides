4118e1e52ca6980aac55c092ec37a6290399c2d3
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-02-27 11:45:47 +0530

    Update Email_sms_blast.php

--- admin/application/modules/email_sms_blast/controllers/Email_sms_blast.php
+++ admin/application/modules/email_sms_blast/controllers/Email_sms_blast.php
@@ -1618,6 +1618,7 @@ class Email_sms_blast extends MX_Controller
         );
         $totalFiltered = $get_data;  
 		$can = 0; 
+		print_r($columns[$requestData['order'][0]['column']]);die;
 		for($c=0;$c<count($requestData['columns']);$c++)
 		{
 			if (!empty($requestData['columns'][$c]['order']['search']['value']))
