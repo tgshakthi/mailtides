67a7810db8b2ad78ad6892adfcccc8a06697858b
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-02-27 15:15:28 +0530

    email sms blast updates

--- admin/application/modules/email_sms_blast/models/Email_sms_blast_model.php
+++ admin/application/modules/email_sms_blast/models/Email_sms_blast_model.php
@@ -67,22 +67,24 @@ class Email_sms_blast_model extends CI_Model
         endif;
         return $records;      
     }
-	/* // Get Patient  Users by Id
+	
+	// Get Patient  Users by Id
     function get_patient_users_by_id($user_id,$provider_name,$facility_name)
     {
         $this->db->select('*');
         $this->db->where(array(
-			'id' => $user_id ,
-			'provider_name' => $provider_name,
-			'facility_name' => $facility_name
+			'id' => $user_id,
+			'is_deleted'=> '0'
         ));
+		$this->db->like('provider_name', $provider_name);
+		$this->db->like('facility_name', $facility_name);
         $query   = $this->db->get($this->table_name);
         $records = array();
         if ($query->num_rows() > 0):
             $records = $query->result();
         endif;
         return $records;      
-    } */
+    }
 	
 	// Get Existing Users
     function get_existing_users()
