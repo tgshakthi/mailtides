d69b720e08bdd63c1a4610e5d1d7c5344a104731
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-02-27 16:16:34 +0530

    Update Email_sms_blast_model.php

--- admin/application/modules/email_sms_blast/models/Email_sms_blast_model.php
+++ admin/application/modules/email_sms_blast/models/Email_sms_blast_model.php
@@ -45,6 +45,7 @@ class Email_sms_blast_model extends CI_Model
 		$this->db->like('provider_name', $provider_name);
 		$this->db->like('facility_name', $facility_name);
         $query   = $this->db->get($this->table_name);
+		print_r($query);
         $records = array();
         if ($query->num_rows() > 0):
             $records = $query->result();
