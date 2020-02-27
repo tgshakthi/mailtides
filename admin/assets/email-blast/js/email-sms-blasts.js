3d0ae2f695afda5e1c980b65e63ece53d7017484
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-02-27 20:25:03 +0530

    Update email-sms-blasts.js

--- admin/assets/email-blast/js/email-sms-blasts.js
+++ admin/assets/email-blast/js/email-sms-blasts.js
@@ -1421,22 +1421,6 @@ $('#phone_number').blur(function () {
 });
 
 $(document).ready(function () {
-	// Datepickers
-	$('#Graphics_min').datepicker({
-		onSelect: function () {
-			table.draw();
-		},
-		changeMonth: true,
-		changeYear: true
-	});
-
-	$('#Graphics_max').datepicker({
-		onSelect: function () {
-			table.draw();
-		},
-		changeMonth: true,
-		changeYear: true
-	});
 	
 	var base_url = $('#base_url').val();
 	$('#table_grid1 .searchbox').each(function () {
