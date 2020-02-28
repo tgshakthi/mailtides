9966a6db616646b25b4e95ee3087ce328d812a35
Arunkumar <57341148+arun@users.noreply.github.com>
2020-02-28 14:34:02 +0530

    Update patient_users.php

--- admin/application/modules/email_sms_blast/views/patient_users.php
+++ admin/application/modules/email_sms_blast/views/patient_users.php
@@ -48,7 +48,7 @@
     <div class="col-lg-12">
         <h2>DataTable using Codeigniter, MySQL and AJAX</h2>                 
     </div>
-</div><!-- /.row -->
+</div>
  
 <div class="row">   
             <div class="col-lg-2">                                        
@@ -93,11 +93,6 @@
         </div>
         </div>        
     </div>
-    <div class="row">
-    <div class="col-lg-12">       
-        <a class="btn btn-info btn-xs" style="margin: 2px" href="https://techarise.com/"><i class="fa fa-mail-reply"></i> Tutorial</a>          
-    </div>
-</div><!-- /.row -->
 							<!--<div class="table_main_container">
 									<table id="table_grid1" class="table table-striped table-bordered dt-responsive nowrap jambo_table bulk_action" width="100%" cellspacing="0">
 										<thead>
