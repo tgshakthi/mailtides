de3aad56677fa6b8bea792095312da8886230c71
Arunkumar <57341148+arun@users.noreply.github.com>
2020-02-28 19:54:17 +0530

    Update email-sms-blasts.js

--- admin/assets/email-blast/js/email-sms-blasts.js
+++ admin/assets/email-blast/js/email-sms-blasts.js
@@ -1842,6 +1842,7 @@ $(document).ready(function () {
         jQuery('#render-list-of-order').html(dataTable);    
         var table = $('#order-datatable').DataTable({
           data: html.data,
+		  "pageLength": 100,
           "bPaginate": true,
           "bLengthChange": true,
           "bFilter": false,
