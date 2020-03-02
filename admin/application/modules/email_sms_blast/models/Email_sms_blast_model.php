1d61c9ac949af4137185218f664bb2fd5dd62198
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-03-02 19:28:25 +0530

    Update Email_sms_blast_model.php

--- admin/application/modules/email_sms_blast/models/Email_sms_blast_model.php
+++ admin/application/modules/email_sms_blast/models/Email_sms_blast_model.php
@@ -1381,7 +1381,7 @@ class Email_sms_blast_model extends CI_Model
 	
 	function get_date_range_graphical_report($campaign_name_data_id,$graphics_min,$graphics_max)
 	{
-		$sql = 'SELECT * from `zcms_import_data` where `campaign_category_id` = ''.$campaign_name_data_id.'' AND `sent_date` BETWEEN ''.$graphics_min.'' AND ''.$graphics_max.''';
+		$sql = "SELECT * from `zcms_import_data` where `campaign_category_id` = ".$campaign_name_data_id." AND `sent_date` BETWEEN ".$graphics_min." AND ".$graphics_max."";
 		$query         = $this->db->query($sql);        
         $totalData     = $query->num_rows();
 		 return $query->result_array();
