e708e27fa8cb21a50081095edb82901486edf735
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-03-09 15:41:47 +0530

    Update add_edit_email_template_generate.php

--- admin/application/modules/email_sms_blast/views/add_edit_email_template_generate.php
+++ admin/application/modules/email_sms_blast/views/add_edit_email_template_generate.php
@@ -400,28 +400,24 @@
          const bothSideBarBtn = document.querySelector('#both-sidebar');
          
          leftSideBarBtn.addEventListener('click', () => {
-         	window.location.href = "http://txgidocs.mailtides.com/admin/email_sms_blast/add_edit_email_template_generate/#left-sidebar";
 			localStorage.clear();
 			window.location.reload();
 			window.location.href = "http://txgidocs.mailtides.com/admin/email_sms_blast/add_edit_email_template_generate/#left-sidebar";
          });
          
          noSideBarBtn.addEventListener('click', () => {
-         	window.location.href = "http://txgidocs.mailtides.com/admin/email_sms_blast/add_edit_email_template_generate/#no-sidebar";
 			localStorage.clear();
 			window.location.reload();
 			window.location.href = "http://txgidocs.mailtides.com/admin/email_sms_blast/add_edit_email_template_generate/#no-sidebar";
          });
          
          rightSideBarBtn.addEventListener('click', () => {
-         	window.location.href = "http://txgidocs.mailtides.com/admin/email_sms_blast/add_edit_email_template_generate/#right-sidebar";
 			localStorage.clear();
 			window.location.reload();
 			window.location.href = "http://txgidocs.mailtides.com/admin/email_sms_blast/add_edit_email_template_generate/#right-sidebar";
          });
          
          bothSideBarBtn.addEventListener('click', () => {
-         	window.location.href = "http://txgidocs.mailtides.com/admin/email_sms_blast/add_edit_email_template_generate/#both-sidebar";
 			localStorage.clear();
 			window.location.reload();
 			window.location.href = "http://txgidocs.mailtides.com/admin/email_sms_blast/add_edit_email_template_generate/#both-sidebar";
