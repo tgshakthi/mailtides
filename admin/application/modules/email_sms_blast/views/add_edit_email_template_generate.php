ca4120ed44b15edd5a9be1c2b44b69beadaaf675
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-03-09 12:56:19 +0530

    Update add_edit_email_template_generate.php

--- admin/application/modules/email_sms_blast/views/add_edit_email_template_generate.php
+++ admin/application/modules/email_sms_blast/views/add_edit_email_template_generate.php
@@ -430,9 +430,7 @@
          function closeNav() {
            document.getElementById("mySidenav").style.width = "0";
          }
-		 $( document ).ready(function() {
-			
-		});
+		
       </script>
    </body>
 </html>
