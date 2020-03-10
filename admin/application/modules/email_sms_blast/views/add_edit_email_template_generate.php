b5ff9d835d9f8d36d86b9875aa6aa8217a2c48af
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-03-10 15:56:27 +0530

    Update add_edit_email_template_generate.php

--- admin/application/modules/email_sms_blast/views/add_edit_email_template_generate.php
+++ admin/application/modules/email_sms_blast/views/add_edit_email_template_generate.php
@@ -316,21 +316,25 @@
          leftSideBarBtn.addEventListener('click', () => {	
 			window.location.href = "http://txgidocs.mailtides.com/admin/email_sms_blast/add_edit_email_template_generate/#left-sidebar";
 			localStorage.clear();
+			window.location.reload();
          });
          
          noSideBarBtn.addEventListener('click', () => {
          	window.location.href = "http://txgidocs.mailtides.com/admin/email_sms_blast/add_edit_email_template_generate/#no-sidebar";
-			localStorage.clear();	
+			localStorage.clear();
+			window.location.reload();			
          });
          
          rightSideBarBtn.addEventListener('click', () => {
 			window.location.href = "http://txgidocs.mailtides.com/admin/email_sms_blast/add_edit_email_template_generate/#right-sidebar";
 			localStorage.clear();	
+			window.location.reload();
          });
          
          bothSideBarBtn.addEventListener('click', () => {
 			window.location.href = "http://txgidocs.mailtides.com/admin/email_sms_blast/add_edit_email_template_generate/#both-sidebar";	
 			localStorage.clear();
+			window.location.reload();
          });
          
          function openNav() {
