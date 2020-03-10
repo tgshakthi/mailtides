62864d5712d7f6ac0091fc574e9af1f41db83723
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-03-10 16:25:57 +0530

    Update add_edit_email_template_generate.php

--- admin/application/modules/email_sms_blast/views/add_edit_email_template_generate.php
+++ admin/application/modules/email_sms_blast/views/add_edit_email_template_generate.php
@@ -317,33 +317,33 @@
 			window.location.href = "http://txgidocs.mailtides.com/admin/email_sms_blast/add_edit_email_template_generate/#left-sidebar";
 			localStorage.clear();
 			refresh();
-			// window.location.reload();
+			window.location.reload();
          });
          
          noSideBarBtn.addEventListener('click', () => {
          	window.location.href = "http://txgidocs.mailtides.com/admin/email_sms_blast/add_edit_email_template_generate/#no-sidebar";
 			localStorage.clear();
 			refresh();
-			// window.location.reload();			
+			window.location.reload();			
          });
          
          rightSideBarBtn.addEventListener('click', () => {
 			window.location.href = "http://txgidocs.mailtides.com/admin/email_sms_blast/add_edit_email_template_generate/#right-sidebar";
 			localStorage.clear();	
 			refresh();
-			// window.location.reload();
+			window.location.reload();
          });
          
          bothSideBarBtn.addEventListener('click', () => {
 			window.location.href = "http://txgidocs.mailtides.com/admin/email_sms_blast/add_edit_email_template_generate/#both-sidebar";	
 			localStorage.clear();
 			refresh();
-			// window.location.reload();
+			window.location.reload();
          });
 		 
          function refresh(){
 			 alert();
-			 window.location.reload();
+			window.location.href = "";
 		 }
          function openNav() {
            document.getElementById("mySidenav").style.width = "260px";
