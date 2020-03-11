dc23da5180e67a03a2ee6c005d98c1962a3ac5de
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-03-11 12:55:16 +0530

    Update editor.js

--- admin/assets/email-blast/js/editor.js
+++ admin/assets/email-blast/js/editor.js
@@ -503,16 +503,16 @@
 					if(typeof idMoved !== 'undefined' && idMoved !== 'undefined')
 					{
 						$.get('http://txgidocs.mailtides.com/admin/assets/email-blast/themes/form-' + idMoved + '.html').done(function(html){
-							var i = '0';
+							var i = '1';
 							moved.html(html).promise().done(function(){
-								i++;
+								
                                 var movedData = moved.html(),
 									type = moved.attr('data-id'),
 									finishing,
 									template = '<table id="'+idMoved+'-'+i+'" align="center" cellpadding="10" border="0" class="ui-draggable ui-draggable-handle editable" style="width:100%; margin:0 auto;" width="100%" data-edit="'+idMoved+'" data-finishing="'+idMoved+'">'+
 										'<tbody>'+
 											'<tr>'+
-												'<td align="left">'+
+												'<td id="'+idMoved+'-'+i+'" align="left">'+
 													init.tooltips + 
 													movedData + 
 												'</td>'+
@@ -545,6 +545,7 @@
 									}
 									$this.find('table[data-finishing^="'+idMoved+'"]').attr('data-finishing',null);
 								});
+								i++;
 							});
 						}).fail(function(a,b,c) {
 							console.log(a,b,c);
