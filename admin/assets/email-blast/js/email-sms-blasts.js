c4bcde4b079db47e61ebedd88209e41c0d02280a
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-03-11 15:50:29 +0530

    Update email-sms-blasts.js

--- admin/assets/email-blast/js/email-sms-blasts.js
+++ admin/assets/email-blast/js/email-sms-blasts.js
@@ -1774,7 +1774,7 @@ jQuery(document).on('click','#date_filter_graphical_report', function(){
 function get_email_template(value)
 {
 	alert(value);
-	if(value = 'email'){
+	if(value == 'email'){
 		alert('test');
 	}
 }
