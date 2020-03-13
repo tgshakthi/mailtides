35bffb005531a1f6c8e612ac6fea817a35aeba9e
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-03-13 11:37:05 +0530

    Update add_edit_campaign_category.php

--- admin/application/modules/email_sms_blast/views/add_edit_campaign_category.php
+++ admin/application/modules/email_sms_blast/views/add_edit_campaign_category.php
@@ -167,7 +167,7 @@
 										</div>
 									</div>
 									<div class="form-group">
-										<label class="control-label col-md-3 col-sm-3 col-xs-12" for="templates" id="template_label">
+										<label class="control-label col-md-3 col-sm-3 col-xs-12" for="templates" id="template_label" style="display: none;">
 											Templates
 											<span class="required">*</span>
 										</label>
