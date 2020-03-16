3c38713be8451e2f531f8e8eb94f785d4f729138
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-03-16 15:27:48 +0530

    Update Email_sms_blast.php

--- admin/application/modules/email_sms_blast/controllers/Email_sms_blast.php
+++ admin/application/modules/email_sms_blast/controllers/Email_sms_blast.php
@@ -953,50 +953,16 @@ class Email_sms_blast extends MX_Controller
 					// Email body content
 					if($campaign_category[0]->campaign_type == 'email'){
 						$mail->Subject= 'Digestive & Liver Disease Consultants , P.A';
-						$mailContent = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-											<html>
-												<head>
-													<meta charset="UTF-8">
-													<meta content="width=device-width, initial-scale=1" name="viewport">
-													<meta name="x-apple-disable-message-reformatting">
-													<meta http-equiv="X-UA-Compatible" content="IE=edge">
-													<meta content="telephone=no" name="format-detection">
-													<title></title>
-													<!--[if (mso 16)]>
-													  <style type="text/css">
-													  a {text-decoration: none;}
-													  .copy{
-														 display : none; 
-													  }	
-													  .save-remove{
-														 display : none; 
-													  }
-													  </style>
-													  <![endif]-->
-													<!--[if gte mso 9]><style>sup { font-size: 100% !important; }</style><![endif]-->
-													<!--[if !mso]><!-- -->
-													<link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,700,700i" rel="stylesheet">
-													<!--<![endif]-->
-												</head>
-												<body>
-													<div class="es-wrapper-color">											
-														<!--[if gte mso 9]>
-															  <v:background xmlns:v="urn:schemas-microsoft-com:vml" fill="t">
-																  <v:fill type="tile" color="#f6f6f6"></v:fill>
-															  </v:background>
-														<![endif]-->';
-														$mailContent .= '<style type="text/css">
-																  .copy{
-																	 display : none; 
-																  }	
-																  .save-remove{
-																	 display : none; 
-																  }
-																  </style>
-																  <script>alert();</script>'.$mail_template;												
-														$mailContent .= '</div>
-												</body>                  
-											</html>';
+						$mailContent = '<style type="text/css">
+										  .copy{
+											 display : none; 
+										  }	
+										  .save-remove{
+											 display : none; 
+										  }
+										  </style>
+										  <script></script>'.$mail_template;												
+														
 					}elseif($campaign_category[0]->campaign_type == 'sms'){
 						$mail->Subject= '';
 						$url = 'http://txgidocs.mailtides.com/admin/email_link_open/sms_email_status/'.$user_ids[$patient_user].'/'.$campaign_category[0]->id.'/'.$track_code.'';
