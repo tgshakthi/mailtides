942d929ccf94df1702abe65a0b7c7d19c3b28f8c
velusamy05 <43366999+velusamy05@users.noreply.github.com>
2020-03-16 15:59:33 +0530

    Update Email_sms_blast.php

--- admin/application/modules/email_sms_blast/controllers/Email_sms_blast.php
+++ admin/application/modules/email_sms_blast/controllers/Email_sms_blast.php
@@ -953,7 +953,8 @@ class Email_sms_blast extends MX_Controller
 					// Email body content
 					if($campaign_category[0]->campaign_type == 'email'){
 						$mail->Subject= 'Digestive & Liver Disease Consultants , P.A';
-						$mailContent = '<div><style>
+						$mailContent = '<div><style></style>
+											<style>
 										  .copy{
 											 display : none; 
 										  }	
